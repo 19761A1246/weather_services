f0b1b7b5128803c4c424fd95f3131b64e9ecb9a2
Priya <rayala.priyadarshini@gmail.com>
2025-06-08 16:53:40 +0530

    Second commit

--- src/main/java/com/example/service/WeatherService.java
+++ src/main/java/com/example/service/WeatherService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class WeatherService {
 
     private WebClient webClient;
@@ -24,7 +26,7 @@ public class WeatherService {
     public Mono<String> getWeather(String city)
     {
         String url=baseUrl+city+"&appid="+apiKey;
-
+        log.info("Inside WeatherService class");
         return webClient.get()
                 .uri(url)
                 .retrieve()
